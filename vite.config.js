038f9f1a8787607fc4facd54b821f20c8e9daa60
Harshitha <harshithababu2316@gmail.com>
2026-01-11 12:56:40 +0530

    Update content

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "Portfolio",
+export default defineConfig(() => {
+  const isGitHub = process.env.VITE_GITHUB === "true";
+
+  return {
+    plugins: [react()],
+    base: isGitHub ? "/Portfolio/" : "/",
+  };
 });
